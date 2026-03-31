1788d798bf644a8f5edabc477ae76cd8e2a3256c
unknown <arjun2508sharma@gmail.com>
2026-03-31 22:37:14 +0530

    updated code

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,27 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../auth.form.scss"
+import { useNavigate, Link } from 'react-router'
+import { useAuth } from "../hooks/useAuth"
 
 const Login = () => {
+
+    const { loading, handleLogin } = useAuth()
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await handleLogin({ email, password })
+        navigate('/')
+    }
+
+    if (loading) {
+        return (<main><h1>Loading......</h1></main>)
+    }
     return (
         <main>
             <div className="form-container">
                 <h1>Login</h1>
 
-                <form >
+                <form onSubmit={handleSubmit}>
 
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="text" id='email' name='email' placeholder='Enter email address' />
+                        <input
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            type="text" id='email' name='email' placeholder='Enter email address' />
                     </div>
 
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="text" id='password' name='password' placeholder='Enter your Password' />
+                        <input
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            type="text" id='password' name='password' placeholder='Enter your Password' />
                     </div>
 
                     <button className='button primary-button'>Login</button>
 
 
                 </form>
+                <p>Don't have an account? <Link to={"/register"} >Register</Link ></p>
             </div>
         </main>
     )
